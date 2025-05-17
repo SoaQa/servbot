dad3993c29658ba02396896e407e27607b5af6b9
soaqa <soaqa57@gmail.com>
2025-05-17 14:25:29 +0300

    ru welcome

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = AsyncTeleBot(os.environ["SERVBOT_TELEGRAM_TOKEN"])
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
-    text = 'Hi, I am ServBot.\nJust write me something and I will repeat it!'
+    text = 'Привет, я ServBot.\nПросто напишите мне что-нибудь, и я повторю это!'
     await bot.reply_to(message, text)
 
 
