54bb0597e8b9d7ecefe2d9ae187a404d6254eba2
soaqa <soaqa57@gmail.com>
2025-06-21 17:14:38 +0300

    refactoring

---
+++ src/handlers/welcome.py
@@ -0,0 +1,7 @@
+from main import bot
+
+
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    text = 'Привет, я ServBot.\nПросто напишите мне что-нибудь, и я повторю это!'
+    await bot.reply_to(message, text)
