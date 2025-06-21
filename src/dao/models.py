20d03e63e71441a4973008d325458f649cd13898
soaqa <soaqa57@gmail.com>
2025-06-21 19:34:21 +0300

    AsyncSessionLocal убрал под if

--- src/dao/models.py
+++ src/dao/models.py
@@ -31,10 +31,11 @@ engine = create_engine(
     echo=True
 )
 
+AsyncSessionLocal = None
 if SERVBOT_ASYNC_DATABASE_URL := os.getenv("SERVBOT_ASYNC_DATABASE_URL"):
     async_engine = create_async_engine(
         SERVBOT_ASYNC_DATABASE_URL,
         echo=True
     )
 
-AsyncSessionLocal = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False) # NoQa
+    AsyncSessionLocal = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False) # NoQa
